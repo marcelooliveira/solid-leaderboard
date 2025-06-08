559f1c1a9a989ba1ebfce50b60ab6b6ae3fe5bce
marcelooliveira <mclricardo@gmail.com>
2025-06-08 20:14:11 -0300

    api scores

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -8,7 +8,7 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <title>Game Leaderboard 20:01</title>
+          <title>Game Leaderboard</title>
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
           <link rel="icon" href="/favicon.ico" />
           {assets}
